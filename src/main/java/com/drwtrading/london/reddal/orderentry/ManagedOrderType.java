7a21e084a6998f649eeaa8e0389f5c6b3d4d2bdd
Dan Cook <dcook@drw.com>
2016-01-28 14:57:48 +0000

    Reduce taker messages

--- src/main/java/com/drwtrading/london/reddal/orderentry/ManagedOrderType.java
+++ src/main/java/com/drwtrading/london/reddal/orderentry/ManagedOrderType.java
@@ -145,7 +145,7 @@ public enum ManagedOrderType {
 
     private static class Constants {
         public static final BookParameters ALLOW_ALL_EXCEPT_STATE_TRANSITION = new BookParameters(true, true, false, true, true);
-        public static final TakingParameters TAKE_BETTER_BY_ONE = new TakingParameters(true, 0, 100, 20, true, 1);
+        public static final TakingParameters TAKE_BETTER_BY_ONE = new TakingParameters(true, 0, 100, 2, true, 1);
         public static final TakingParameters NO_TAKING = new TakingParameters(false, 0, 0, 0, false, 0);
         public static final int NO_BETTERMENT = 0;
         public static final int BETTER_BY_ONE = 1;
