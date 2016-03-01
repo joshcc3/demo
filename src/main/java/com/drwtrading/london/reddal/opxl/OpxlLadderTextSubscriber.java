e5d5a053f912441050ba4f83d617cd63556cb1b7
Dan Cook <dcook@drw.com>
2016-03-01 14:02:50 +0000

    Disable r1c5

--- src/main/java/com/drwtrading/london/reddal/opxl/OpxlLadderTextSubscriber.java
+++ src/main/java/com/drwtrading/london/reddal/opxl/OpxlLadderTextSubscriber.java
@@ -25,17 +25,12 @@ public class OpxlLadderTextSubscriber {
             "r1c2",
             "r1c3",
             "r1c4",
-            "r1c5",
             "r2c1",
             "r2c2",
             "r2c3",
             "r2c4",
-            "r2c5",
-            "r3c1",
-            "r3c2",
-            "r3c3",
-            "r3c4",
-            "r3c5");
+            "r2c5"
+    );
 
     public OpxlLadderTextSubscriber(Publisher<Throwable> errorPublisher, Publisher<LadderMetadata> publisher) {
         this.errorPublisher = errorPublisher;
