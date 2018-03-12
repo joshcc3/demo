158913bc26e3771c66e93845dfaca31358967c34
Dan Cook <dcook@drw.com>
2018-03-12 12:27:24 +0000

    Add 0.5 bps and change some child offsets

--- src/main/java/com/drwtrading/london/reddal/stacks/family/ChildOffsetCalculator.java
+++ src/main/java/com/drwtrading/london/reddal/stacks/family/ChildOffsetCalculator.java
@@ -11,26 +11,26 @@ public final class ChildOffsetCalculator {
     static {
         EXCHANGE_OFFSETS = new HashMap<>();
 
-        EXCHANGE_OFFSETS.put("AV", 5d);
-        EXCHANGE_OFFSETS.put("BB", 1.1d);
-        EXCHANGE_OFFSETS.put("DC", 1.55d);
+        EXCHANGE_OFFSETS.put("AV", 5.5d);
+        EXCHANGE_OFFSETS.put("BB", 1.6d);
+        EXCHANGE_OFFSETS.put("DC", 2.05d);
         EXCHANGE_OFFSETS.put("EB", 3d);
-        EXCHANGE_OFFSETS.put("FH", 1.55d);
-        EXCHANGE_OFFSETS.put("FP", 1.1d);
-        EXCHANGE_OFFSETS.put("GY", 1.1d);
-        EXCHANGE_OFFSETS.put("ID", 1.25d);
-        EXCHANGE_OFFSETS.put("IM", 3d);
-        EXCHANGE_OFFSETS.put("IX", 0.75d);
-        EXCHANGE_OFFSETS.put("LN", 1.1d);
-        EXCHANGE_OFFSETS.put("NA", 1.1d);
-        EXCHANGE_OFFSETS.put("NO", 1.95d);
-        EXCHANGE_OFFSETS.put("RFQ", 0d);
-        EXCHANGE_OFFSETS.put("PA", 1.1d);
-        EXCHANGE_OFFSETS.put("PL", 1.1d);
+        EXCHANGE_OFFSETS.put("FH", 2.05d);
+        EXCHANGE_OFFSETS.put("FP", 1.6d);
+        EXCHANGE_OFFSETS.put("GY", 1.6d);
+        EXCHANGE_OFFSETS.put("ID", 1.75d);
+        EXCHANGE_OFFSETS.put("IM", 1.6d);
+        EXCHANGE_OFFSETS.put("IX", 1.25d);
+        EXCHANGE_OFFSETS.put("LN", 1.6d);
+        EXCHANGE_OFFSETS.put("NA", 1.6d);
+        EXCHANGE_OFFSETS.put("NO", 2.45d);
+        EXCHANGE_OFFSETS.put("RFQ", 0.0d);
+        EXCHANGE_OFFSETS.put("PA", 1.6d);
+        EXCHANGE_OFFSETS.put("PL", 1.6d);
         EXCHANGE_OFFSETS.put("SE", 3d);
-        EXCHANGE_OFFSETS.put("SS", 1.55d);
-        EXCHANGE_OFFSETS.put("UF", 0.75d);
-        EXCHANGE_OFFSETS.put("UW", 0.75d);
+        EXCHANGE_OFFSETS.put("SS", 2.05d);
+        EXCHANGE_OFFSETS.put("UF", 1.25d);
+        EXCHANGE_OFFSETS.put("UW", 1.25d);
     }
 
     static double getSymbolOffset(final String symbol) {
